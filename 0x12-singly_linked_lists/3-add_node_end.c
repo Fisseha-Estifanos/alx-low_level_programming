e919e6628828189a2f7b56c57f88f3fb8e8b8c0b
fish <fisseha.137@gmail.com>
2022-07-15 17:13:24 +0300

    0x12: modified add node end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,63 +1,42 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
 /**
-  * add_node_end - Adds a new node at the end of a list
-  * @head: The original linked list
-  * @str: The string to add to the node
-  *
-  * Return: The address of the new list or NULL if it failed
-  */
+ * add_node_end - adds a new node to the end
+ * of a singly linked list
+ * @head: a pointer to the address of the
+ * beginning of the list_t list
+ * @str: pointer to the string to add
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_list, *temp;
+	unsigned int len = 0;
+	list_t *new_node, *temp;
 
-	if (str != NULL)
+	/* get length of string */
+	while (*(str + len))
+		len++;
+
+	/* allocate memory to heap */
+	new_node = malloc(sizeof(size_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->len = len;
+	/* Locate the end of list_t: if empty append new_node */
+	if (*head == NULL)
 	{
-		new_list = malloc(sizeof(list_t));
-		if (new_list == NULL)
-			return (NULL);
-
-		new_list->str = strdup(str);
-		new_list->len = _strlen(str);
-		new_list->next = NULL;
-
-		if (*head == NULL)
-		{
-			*head  = new_list;
-			return (*head);
-		}
-		else
-		{
-			temp = *head;
-			while (temp->next)
-				temp = temp->next;
-
-			temp->next = new_list;
-			return (temp);
-		}
+		*head = new_node;
+		return (new_node);
 	}
+	temp = *head;
+	/* esle iterate to the last node */
+	while (temp->next)
+		temp = temp->next;
+	/* append new_node */
+	temp->next = new_node;
 
-	return (NULL);
-}
-
-/**
-  * _strlen - Returns the length of a string
-  * @s: String to count
-  *
-  * Return: String length
-  */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (*s)
-	{
-		s++;
-		c++;
-	}
-
-	return (c);
+	return (new_node);
 }
